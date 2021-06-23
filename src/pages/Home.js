1c96fb945604277ec3fb34eb5baec51e5971b71c
Daniel Um <39230120+umdaniel@users.noreply.github.com>
2021-06-23 15:44:18 -0400

    Skeleton for services section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
 import HeroSection from '../components/HeroSection';
+import AboutSection from '../components/AboutSection';
+import ServiceSection from '../components/ServiceSection';
 
 export default function () {
     return (
         <div>
             <HeroSection />
+            <AboutSection />
+            <ServiceSection />
         </div>
     );
 }
