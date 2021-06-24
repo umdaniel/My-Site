806c0cd9d80cb1b8f3c6f47fba95acc87b14ae03
Daniel Um <39230120+umdaniel@users.noreply.github.com>
2021-06-24 14:36:14 -0400

    Projects, contact me, and footer completed

--- src/components/ServiceSectionItem.js
+++ src/components/ServiceSectionItem.js
@@ -2,19 +2,34 @@ import React from 'react'
 
 import { MdDesktopMac } from 'react-icons/md';
 import PText from './PText';
+import styled from 'styled-components';
 
-export default function ServiceSectionItem() {
+const ItemStyles = styled.div`
+text-align: center;
+.servicesItem__icon {
+    svg {
+        width: 3rem;
+    }
+}
+.servicesItem__title {
+    font-size: 2.5rem;
+    font-family: 'Montserrat SemiBold';
+}
+.para {
+    margin-top: 2rem;
+}
+`;
+
+export default function ServiceSectionItem({
+    icon = <MdDesktopMac />,
+    title= 'web design',
+    desc='creating elegant user interfaces on the web'
+}) {
     return (
-        <div>
-            <div className="servicesItem__icon">
-                <MdDesktopMac />
-                <div className="servicesItem__title">
-                    Web Design
-                </div>
-                <PText>
-                    Web Development
-                </PText>
-            </div>
-        </div>
+        <ItemStyles>
+            <div className="servicesItem__icon">{icon}</div>
+            <div className="servicesItem__title">{title}</div>
+            <PText>{desc}</PText>
+        </ItemStyles>
     );
 }
