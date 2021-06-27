9f4d9c4ec52e9f03199570f5a39dd44ff71e2b14
Daniel Um <39230120+umdaniel@users.noreply.github.com>
2021-06-26 20:32:59 -0400

    Completed Website
    needs updated pictures, updated css, and updated links to projects / resume

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -9,6 +9,46 @@ import styled from 'styled-components';
 
 const ContactSectionStyle = styled.div`
 padding: 10rem 0;
+.contactSection__wrapper {
+    display: flex;
+    gap: 5rem;
+    margin-top: 7rem;
+    justify-content: space-between;
+    position: relative;
+}
+.contactSection__wrapper::after {
+    position: absolute;
+    content: '';
+    width: 2px;
+    height: 50%;
+    background-color: var(--gray-1);
+    left: 50%;
+    top: 30%;
+    transform: translate(-50%, -50%);
+}
+.left {
+    width: 100%;
+    max-width: 500px;
+}
+.right {
+    max-width: 500px;
+    width: 100%;
+}
+@media only screen and (max-width: 768px) {
+    .contactSection__wrapper {
+        flex-direction: column;
+    }
+    .contactSeciton__wrapper::after {
+        display: none;
+    }
+    .left,
+    .right {
+        max-width: 100%;
+    }
+    .right {
+        paddding: 4rem 2rem 2rem 2rem;
+    }
+}
 `;
 
 export default function ContactSection() {
